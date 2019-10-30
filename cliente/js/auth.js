7673923108a1d26e3bb66761c4a0b94ce0e1a8ed
dgutierrezd <danielgudu22@gmail.com>
2019-10-29 19:51:07 -0500

    Validando signin y signup

--- cliente/js/auth.js
+++ cliente/js/auth.js
@@ -4,22 +4,43 @@ const registrar = () => {
     let perfil = document.getElementById('perfil').value;
     let pass = document.getElementById('contraseña').value;
 
-    let usuario = {
-        nombre,
-        correo,
-        perfil,
-        pass
+    if(nombre == '' || correo == '' || pass == '') {
+        //alert('No has ingresado las credenciales completas.')
+
+        swal.fire({
+            title: 'Error!',
+            text: 'Llena todos los espacios',
+            type: 'error',
+            confirmButtonText: 'Cool'
+        })
+    } else {
+        let usuario = {
+            nombre,
+            correo,
+            perfil,
+            pass
+        }
+    
+        console.log(usuario)
     }
-    console.log(usuario)
 }
 
 const iniciarSesion = () => {
     let correo = document.getElementById('correo').value;
     let pass = document.getElementById('password').value;
 
-    let usuario = {
-        correo,
-        pass
+    if(correo == '' || pass == '') {
+        swal.fire({
+            title: 'Error!',
+            text: 'Llena todos los espacios',
+            type: 'error',
+            confirmButtonText: 'Cool'
+        })
+    } else {
+        let usuario = {
+            correo,
+            pass
+        }
+        console.log(usuario);
     }
-    console.log(usuario);
 }
